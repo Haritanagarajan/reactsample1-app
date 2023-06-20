3fde63f19aab04564392be8cd9fde3359cb1981f
Haritanagarajan <30harita2002@gmail.com>
2023-06-20 12:07:48 +0530

    REACT

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+const About = () => {
+    const [namesindex, setNamesindex] = useState([]);
+    const [shownames, setNames] = useState(null);
 
-
-
-  const About = () => {
-    const[namesindex, setNamesindex] = useState([]);
+    function toggle(names) {
+        if (shownames && shownames.id === names.id) {
+            setNames(null);
+        } else {
+            setNames(names);
+        }
+    }
 
     const fetchData = () => {
-        fetch("http://localhost:4000/users")
-            .then((response) => {
-                return response.json();
-            })
+        fetch('http://localhost:4000/users')
+            .then((response) => response.json())
             .then((data) => {
                 setNamesindex(data);
             });
@@ -21,31 +24,62 @@ import { useEffect, useState } from 'react';
         fetchData();
     }, []);
 
-
     return (
         <div style={{ marginLeft: 150, marginTop: 100 }}>
+
             {namesindex.length > 0 && (
 
-
                 <div className="row">
+
                     {namesindex.map((names) => (
 
-                        <div className="col-md-3" key={names.id} style={{ border: 'inset', width: 150, borderRadius: 10, marginLeft: 50, marginBottom: 30 }}>
-                            <div className="card" style={{ border: 'none', marginTop: 10 }}>
-                                <div className="card-head">
-                                    <img
-                                        src={names.url}
-                                        alt={names.name}
-                                        height={100}
-                                        width={100}
-                                    />
+
+
+                        <div
+                            className="col-md-3"
+                            key={names.id}
+                            style={{
+                                width: 150,
+                                borderRadius: 10,
+                                marginLeft: 50,
+                                marginBottom: 30,
+                            }}
+                        >
+                            <div  style={{ border: 'none', marginTop: 10 }}>
+
+                                <div className="card">
+
+                                    <button onClick={() => toggle(names)} className="btn">
+
+                                        {shownames && shownames.id === names.id
+                                            ? 'LESS INFO'
+                                            : 'MORE INFO'}
+
+                                        {shownames && shownames.id === names.id && (
+                                            <div className='card' style={{border:'inset'}}>
+                                                <p style={{ color: 'black', fontWeight: '500' }}>
+                                                    {names.name}
+                                                    <br />
+                                                    {names.role}
+                                                    <br />                                                    {names.experience}
+
+                                                </p>
+                                            </div>
+                                        )}
+                                    </button>
+
                                 </div>
+
+                                <img src={names.url} alt={names.name} height={100} width={100} style={{marginLeft:'10px'}}/>
+
                             </div>
-                            <div className="card-body">{names.experience} <br/>{names.role}</div>
                         </div>
                     ))}
+
+
                 </div>
             )}
+
         </div>
     );
 };
